627d284f9568ead5755d6359a93e123d86b4ce8e
AlleHunter <allehunter@tutamail.com>
2025-06-23 10:41:12 +0200

    Update app2.py

--- app2.py
+++ app2.py
@@ -156,7 +156,7 @@ with st.expander("Jak przygotować dane i używać AlleHunter?"):
         """
         ### Witaj w Panelu Aplikacji **AlleHunter**
         
-        Aplikacja jest **ZUPEŁNIE DARMOWYM** narzędziem analitycznym, powstałym, aby umożliwić Sprzedającym na portalu Allegro szybkie sprawdzenie poprawności naliczonych przez serwis opłat z tytułu podjazdów kurierskich DPD. Stanowi odpowiedź na przeszkody stawiane przed Sprzedającymi w zakresie sprawdzania udotępnianych im zestawień i raportów, wyrównuje szanse w zakresie analizy danych i eliminuje konieczność wielotygodniowego, ręcznego rozpisywania i zestawiania tysięcy rekordów.
+        Aplikacja jest **ZUPEŁNIE DARMOWYM** narzędziem analitycznym, powstałym, aby umożliwić Sprzedającym na portalu Allegro szybkie sprawdzenie poprawności naliczonych przez serwis opłat z tytułu podjazdów kurierskich DPD. Stanowi odpowiedź na przeszkody stawiane przed Sprzedającymi w zakresie sprawdzania udostępnianych im zestawień i raportów, wyrównuje szanse w zakresie analizy danych i eliminuje konieczność wielotygodniowego, ręcznego rozpisywania i zestawiania tysięcy rekordów.
         
         Działanie aplikacji opiera się na grupowaniu wszystkich przesyłek DPD, niezależnie od rodzaju, z podziałem na dni i sprawdzeniu, czy w danym dniu naliczono opłatę za pojazd kuriera, oraz czy jest to uzasadnione. Działa zarówno w przypadku, gdy sprzedajesz na jednym koncie, ale również w odniesniu do wielu kont sprzedażowych, łącząc dane z dowolnej ich liczby, co jest absolutnie fundamentalne, gdy chcemy uzyskać poprawne dane wynikowe.
         
